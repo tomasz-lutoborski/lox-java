9f040a1abc3d5c50491612d7f6862d1bd1ba8099
tomasz-lutoborski <tomek218@hotmail.com>
2023-03-15 12:59:04 +0100

    initial commit

---
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -0,0 +1,27 @@
+package com.craftinginterpreters.lox;
+
+class Interpreter implements Expr.Visitor<Object> {
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        return null;
+    }
+}
